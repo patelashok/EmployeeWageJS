82aed1a75fd88c22f6ef174e1184923e1bb0c25c
patelashok <ashok.patel2917@gmail.com>
2021-06-26 13:17:38 +0530

    calculate empwage 20 Days

--- EmpWage.js
+++ EmpWage.js
@@ -1,3 +1,10 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_Hours = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAY = 20;
+
 function getWorkingHours(empCheck) {
     switch(empCheck) {
         case IS_PART_TIME:
@@ -8,15 +15,12 @@ function getWorkingHours(empCheck) {
                     return 0;
     }
     }
-    
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_Hours = 8;
-    const WAGE_PER_HOUR = 20;
-    let empHrs = 0;
-    
-    let empCheck = Math.floor(Math.random() * 10) %3;
-    empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Hour: " +empHrs+ " Emp Wage: " + empWage);
+
+let totalEmpHrs = 0;
+for( let day = 0; day < NUM_OF_WORKING_DAY; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Hour: " +totalEmpHrs+ " Emp Wage: " + empWage);
